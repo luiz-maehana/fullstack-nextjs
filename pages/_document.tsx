2320a76978a045b443266ec6ea2bb774ca1c2d7b
Luiz Fernando Maehana <luizmaehana@gmail.com>
2024-02-22 18:02:34 -0300

    Refactor GTAG

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,8 @@ import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/do
 import { ServerStyleSheet } from 'styled-components'
 import { GoogleAnalytics } from '@next/third-parties/google'
 
+const gtag = `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`;
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
@@ -28,11 +30,23 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script async src={gtag} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                  page_path: window.location.pathname
+                });
+              `,
+            }}
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
         </Head>
-        <GoogleAnalytics gaId={process.env.gtag}/>
         <body>
           <Main />
           <NextScript />
