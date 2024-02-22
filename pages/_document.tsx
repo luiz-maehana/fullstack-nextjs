2486b1ca7a193e12db1f80a2839d8fd9545ed796
Luiz Fernando Maehana <luizmaehana@gmail.com>
2024-02-22 17:52:16 -0300

    Add Google Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -31,6 +32,8 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
         </Head>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.gtag}`}></script>
+        <GoogleAnalytics gaId={process.env.gtag}/>
         <body>
           <Main />
           <NextScript />
